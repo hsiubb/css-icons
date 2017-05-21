0f07ee6cbe4c2282ef387e0f02215351c8151870
hsiubb <479833352@qq.com>
2017-05-21 12:53:41 +0800

    update

--- js/css-icons.js
+++ js/css-icons.js
@@ -1,6 +1,6 @@
 (function($) {
   $(document).ready(function($) {
-    $('[class*="css-icon-"]').closest('a').click(function() {
+    $('.css-icon').click(function() {
       var $icon = $(this).find('[class*="css-icon-"]');
       if($icon.attr('data-icon')) {
         var setClass = $icon.prop('class').match(/css-icon(-\w+)+/gy)[0].replace('css-icon-','');
