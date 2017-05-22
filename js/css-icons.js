13beb1e6118fe50a94d84fbed8c2de1b2fec2ea5
hsiubb <479833352@qq.com>
2017-05-22 23:02:17 +0800

    update

--- js/css-icons.js
+++ js/css-icons.js
@@ -1,22 +1,25 @@
-(function($) {
-	$.iconCallBack = {};
+var iconCallBack = {};
+(function(iconCallBack) {
+	let css_icon = document.querySelectorAll('.css-icon');
+	for(let i=0; i<css_icon.length; i++) {
+	  let icon = css_icon[i].querySelector('[class*="css-icon-"]');
+		css_icon[i].addEventListener('click', function(evnt) {
+			let icon_cur = icon.getAttribute('class').match(/css-icon(-\w+)+/g)[0].replace('css-icon-','');
+			let icon_next = icon.getAttribute('data-icon');
+			let icon_calbak = icon.getAttribute('data-calbak');
+			let change_icon = !!icon_next;
 
-  $(document).ready(function($) {
-    $('.css-icon').click(function() {
-      var $icon = $(this).find('[class*="css-icon-"]');
-      if($icon.attr('data-callback')) {
-        $.iconCallBack[$icon.attr('data-callback')](
-          $icon,
-          $icon.prop('class').match(/css-icon(-\w+)+/gy)[0].replace('css-icon-',''),
-          $icon.attr('data-icon') || false
-        );
-      }
-      if($icon.attr('data-icon')) {
-        var setClass = $icon.prop('class').match(/css-icon(-\w+)+/gy)[0].replace('css-icon-','');
-        $icon.removeClass();
-        $icon.addClass('css-icon-'+$icon.attr('data-icon'));
-        $icon.attr('data-icon',setClass);
-      }
-    });
-  });
-}(jQuery));
+			if(icon_calbak) {
+				if(iconCallBack[icon_calbak]) {
+					change_icon = iconCallBack[icon_calbak](evnt, icon, icon_cur);
+				} else {
+					throw('function iconCallBack[' + icon_calbak + '] is not exist');
+				}
+			}
+			if(change_icon) {
+				icon.setAttribute('class', 'css-icon-'+icon_next);
+				icon.setAttribute('data-icon', icon_cur);
+			}
+		});
+	}
+}(typeof iconCallBack != 'undefined' ? iconCallBack : {}));
