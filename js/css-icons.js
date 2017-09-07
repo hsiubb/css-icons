7b6ddcf9641bcb912c2e3c62835cc55c9af13df6
Unknown <479833352@qq.com>
2017-09-08 00:01:16 +0800

    update to vue components

--- js/css-icons.js
+++ js/css-icons.js
@@ -1,6 +1,8 @@
 let css_icon_vm = new Vue({
-	el: '#icons',
+	el: '#css-icon',
 	data: {
+		elementsTitle: 'Elements',
+		groupsTitle: 'Groups',
 		iconsList: ['app', 'grid', 'menu', 'close', 'tick', 'attention', 'question', 'pencil', 'user', 'avatar', 'home', 'card', 'position', 'search', 'chat', 'bubble', 'drop', 'focus', 'laptop', 'monitor', 'arrowkey', 'mobile', 'file', 'picture', 'book', 'envelop', 'trash', 'clock'],
 		groupList: [
 			['loading-1', 'loading-2', 'yinyang'],
@@ -17,37 +19,71 @@ let css_icon_vm = new Vue({
 			['sword', 'katana', 'axe', 'double-bit-axe', 'hammer', 'knife', 'kitchen-knife', 'armor', 'shield', 'shield2', 'boot', 'target'],
 			['sack', 'potions-lg', 'potions-md', 'potions-sm', 'syrup'],
 			['aircraft', 'fighter', 'tank', 'tank2', 'tank3', 'tank4']
+		],
+		lines: [
+			`"data-icon" sets icons' next style, without this, the icon will not change its style when you click it.`,
+			`"data-calbak" set functions which run after you click the icon, contains 3 parameters: event type, icon's HTML node, class suffix before click event, return false in the function will stop the change of icons.`,
+			`PS: Icons lock, unlock, heart, love in this page all binded function iconCallBack["view"] which will output some text in the console.`
 		]
 	},
 	components: {
 		'search-bar': {
-			props: ['iconInput'],
 			template: '<div class="search-bar clrfix">\
 				<a class="css-icon" href="javascript:;">\
 					<i id="css-icon" :class="`css-icon-`+(iconInput||`search`)"></i>\
 				</a>\
-				<input v-model="iconInput" class="icon-input" type="text" autocomplete="off" placeholder="请输入图标名称进行预览，如:\'menu\'" />\
+				<input v-model="iconInput" class="icon-input" @focus="clearValue" @blur="resetValue" type="text" autocomplete="off" placeholder="Enter icons name to search them, e.g.: \'app\'" />\
 			</div>',
-			data:function() {
+			data: function() {
 				return {
-					iconInput: 'search'
+					'iconInput': 'search'
+				}
+			},
+			methods: {
+				clearValue: function() {
+					this.iconInput = '';
+				},
+				resetValue: function() {
+					this.iconInput = 'search';
 				}
 			}
 		},
-		guide: {
+		'guide': {
 			template: `
 				<div class="container guide">
 					<h2>Descriptions:</h2>
 					Icons behavior depend on it's HTML attribute,
-					<p>"data-icon" sets icons' next style, without this, the icon will not change its style when you click it.</p>
-					<p>"data-calbak" set functions which run after you click the icon, contains 3 parameters: event type, icon's HTML node, class suffix before click event, return false in the function will stop the change of icons.</p>
-					<p>PS: Icons lock, unlock, heart, love in this page all binded function iconCallBack["view"] which will output some text in the console.</p>
+					<p v-for="line in lines">{{line}}</p>
 				</div>
-			`
+			`,
+			data: function() {
+				return {
+					lines: [
+						`<css-icon> has two props, ':icon-name' and ':group'. one for display, the other one is the name of icons group, which the icon will turn it's class to when being click.`,
+						`click again will turn icons class to default.`
+					]
+				}
+			}
 		},
 		'css-icon': {
-			props: ['iconName'],
-			template: '<a class="css-icon" href="javascript:;"><i :class="`css-icon-`+iconName"></i><span class="icon-name">{{iconName}}</span></a>'
+			props: ['iconName', 'group'],
+			template: '<a class="css-icon" href="javascript:;" @click="toggle"><i :class="`css-icon-`+(iconTarget||iconName)"></i><span class="icon-name">{{iconName}}</span></a>',
+			data: function() {
+				return {
+					iconTarget: undefined
+				}
+			},
+			methods: {
+				toggle: function() {
+					if(this.group) {
+						let len;
+						do {
+							len = Math.floor(this.group.length * Math.random());
+						} while(this.group[len] === this.iconName);
+						this.iconTarget = this.iconTarget ? undefined : this.group[len];
+					}
+				}
+			}
 		}
 	}
 });
